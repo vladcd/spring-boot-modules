d9ec19930ccc173a724f6a3fa619671cd58230b6
Vlad Carcu <vlad.carcu@softvision.com>
2020-06-12 17:43:21 +0300

    - updated Actor structure

--- domain/src/main/java/com/example/modules/domain/entities/Actor.java
+++ domain/src/main/java/com/example/modules/domain/entities/Actor.java
@@ -2,7 +2,16 @@ package com.example.modules.domain.entities;
 
 public class Actor {
 
+    private Integer id;
     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
